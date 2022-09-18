4b8b5080f331542e5092ba965202f37f8e1b4e89
Light-Lens <73633374+Light-Lens@users.noreply.github.com>
2022-09-18 12:15:11 +0530

    Upload CalcSqrt.py

---
+++ CalcSqrt.py
@@ -0,0 +1,15 @@
+import sys
+
+def sqrt(x, y): return (y**2 + x) / (2 * y)
+print("Type a number you want to find the square root of otherwise type 'quit' to exit.")
+
+ans = 1
+prev_ans = 0
+input_num = input("> ")
+sys.exit() if input_num.lower() == "quit" else None
+for _ in range(1000):
+	ans = sqrt(int(input_num), ans)
+	if prev_ans == ans: break
+
+	prev_ans = ans
+	print(ans)
